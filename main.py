47e2a1c5c07ac771a4cc38806d5e27876bdd0ef0
NKpba <emailrealnidhi@gmail.com>
2026-04-18 19:23:03 -0400

    Added xlsx file and number of serotypes code

--- main.py
+++ main.py
@@ -4,4 +4,15 @@
     # Use a breakpoint in the code line below to debug your script.  # Press ⌘F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Provided Libraries
 
+import pandas as pd                 # For Excel File, also need openpyxl
+import matplotlib.pyplot as plt     # Plotting
+import numpy as np                  # Calculations
+
+
+excel_file = "BEAM_Dashboard_-_Serotypes_of_concern__Illnesses_and_Outbreaks_20260418.xlsx"
+df = pd.read_excel("BEAM_Dashboard_-_Serotypes_of_concern__Illnesses_and_Outbreaks_20260418.xlsx")
+serotypes = df["Serotype"].dropna().unique()
+#print(serotypes)
+print("Number of serotypes:", len(serotypes))
