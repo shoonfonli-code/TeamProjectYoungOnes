0feb12b57a6004e71486fc6cf84b8a95323966e2
NKpba <emailrealnidhi@gmail.com>
2026-04-18 19:35:18 -0400

    added graph of sum no.illness over time for each serotype

--- main.py
+++ main.py
@@ -16,3 +16,35 @@ df = pd.read_excel("BEAM_Dashboard_-_Serotypes_of_concern__Illnesses_and_Outbrea
 serotypes = df["Serotype"].dropna().unique()
 #print(serotypes)
 print("Number of serotypes:", len(serotypes))
+
+
+
+###############################################################################
+Sumalltotals = df.groupby("Serotype")["No_of_illnesses"].sum()
+
+hightolowtotals = Sumalltotals.sort_values(ascending=False).head(10)
+print(hightolowtotals)
+
+
+
+#############################################################################
+# sum illnesses by serotype & year
+yearlytotals = df.groupby(["Serotype", "Year_first_ill"])["No_of_illnesses"].sum().reset_index()
+top10 = hightolowtotals.index
+# keep only those rows
+top10data = yearlytotals[yearlytotals["Serotype"].isin(top10)]
+
+#Plotting the graph of the top 10
+plt.figure(figsize=(14,8))
+for name, group in top10data.groupby("Serotype"):
+    group = group.sort_values("Year_first_ill")
+    plt.plot(group["Year_first_ill"], group["No_of_illnesses"], marker="o", label=name)
+
+plt.xlabel("Year")
+plt.ylabel("Total Illnesses")
+plt.title("Number of Illnesses for the Top 10 Serotypes across time")
+plt.legend()
+plt.legend(bbox_to_anchor=(0.86, 1), loc="upper left")
+plt.grid(True)
+plt.show()
+###############################################################################################
