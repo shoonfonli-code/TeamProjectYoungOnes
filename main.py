fe06d170470c5637f4d5f0dbd3a647afaf398343
NKpba <emailrealnidhi@gmail.com>
2026-04-18 20:04:34 -0400

    Added 7

--- main.py
+++ main.py
@@ -4,31 +4,61 @@
     # Use a breakpoint in the code line below to debug your script.  # Press ⌘F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# Provided Libraries
 
+# Provided Libraries
 import pandas as pd                 # For Excel File, also need openpyxl
 import matplotlib.pyplot as plt     # Plotting
 import numpy as np                  # Calculations
 
-
 excel_file = "BEAM_Dashboard_-_Serotypes_of_concern__Illnesses_and_Outbreaks_20260418.xlsx"
 df = pd.read_excel("BEAM_Dashboard_-_Serotypes_of_concern__Illnesses_and_Outbreaks_20260418.xlsx")
+
+#Step 2/3: Aggregate by Serotype # Record Data
 serotypes = df["Serotype"].dropna().unique()
-#print(serotypes)
 print("Number of serotypes:", len(serotypes))
 
+###############################################################################################
+#Step 4: Sort by Year
+#To determine which is the most FREQUENT, as in, most times of no_of_illness not being 0
+nonzero = df[df["No_of_illnesses"] > 0]
+freq = nonzero["Serotype"].value_counts().head(10) #Count how many times each serotype appears
+print(freq)
+#We can see expectedly that Enteritidis is the most frequently reported illness irrespective of meat
+####################################################################################
+#Step 5: Trend Analysis
+# sum illnesses for each serotype in each year
+yearlytotals = df.groupby(
+    ["Serotype", "Year_first_ill"]
+)["No_of_illnesses"].sum().reset_index()
+
+# #Plotting the graph
+plt.figure(figsize=(14,8))
+for name, group in yearlytotals.groupby("Serotype"):
+    group = group.sort_values("Year_first_ill")
+    plt.plot(group["Year_first_ill"], group["No_of_illnesses"], marker="o", label=name)
 
 
-###############################################################################
+plt.xlabel("Year")
+plt.ylabel("Total Illnesses")
+plt.title("Number of Illnesses for each Serotypes across time")
+plt.legend()
+#the legend is covering up the grid
+plt.legend(bbox_to_anchor=(1.02, 1), loc="upper left")
+plt.grid()
+plt.show()
+
+
 Sumalltotals = df.groupby("Serotype")["No_of_illnesses"].sum()
 
-hightolowtotals = Sumalltotals.sort_values(ascending=False).head(10)
+hightolowtotals = Sumalltotals.sort_values(ascending=False)
 print(hightolowtotals)
 
+##############################################################################################
+#Step 6:Top 10 Over Time
 
-
-#############################################################################
 # sum illnesses by serotype & year
+hightolowtotals = Sumalltotals.sort_values(ascending=False).head(10)
+print(hightolowtotals)
 yearlytotals = df.groupby(["Serotype", "Year_first_ill"])["No_of_illnesses"].sum().reset_index()
 top10 = hightolowtotals.index
 # keep only those rows
@@ -47,4 +77,13 @@ plt.legend()
 plt.legend(bbox_to_anchor=(0.86, 1), loc="upper left")
 plt.grid(True)
 plt.show() #shows graph
+
 ###############################################################################################
+#Step 7: Resort by Serotype
+meat_serotype = df.groupby(["Serotype", "Food_category"])["No_of_illnesses"].sum().reset_index()
+pivot = meat_serotype.pivot(
+    index="Serotype",
+    columns="Food_category",
+    values="No_of_illnesses").fillna(0)
+# A pivot basically reshapes the data into a new data table that can be used to make a bar graph
+##############################################################################################
